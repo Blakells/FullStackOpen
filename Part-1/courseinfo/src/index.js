049e113124f27a1729c42ab24bca9a4bd91c7686
Blakells <blake.bailey09@gmail.com>
2020-04-15 15:23:10 -0400

    update

--- Part-1/courseinfo/src/index.js
+++ Part-1/courseinfo/src/index.js
@@ -27,21 +27,25 @@ const Total = ({total}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-  {
+  const course = {
+    name: 'Half Stack application development',
+   parts: [
+    {
     name: 'Fundamentals of React',
     exercises: 10
-  },
-   {
+    },
+    {
     name: 'Using props to pass data',
     exercises: 7
-  },
-   {
+    },
+    {
     name: 'State of a component',
     exercises: 14
-  }
-]
+    }
+  ]
+}
+
+const parts = course.parts
 
   const Content = (props) => {
     return (
@@ -54,6 +58,7 @@ const App = () => {
   }
 
   return (
+
     <div>
       <Header/>
       <Content/>
