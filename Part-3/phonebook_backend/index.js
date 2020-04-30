3a1b4e16cf967ebeb1d5357781b4f101446420f0
Blakells <blake.bailey09@gmail.com>
2020-04-30 15:15:47 -0400

    part 3a

--- Part-3/phonebook_backend/index.js
+++ Part-3/phonebook_backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-
+// const header = morgan(function (tokens, req, res) {
+//   return [
+//     tokens.method(req, res),
+//     tokens.url(req, res),
+//     tokens.status(req, res),
+//     tokens.res(req, res, 'content-length'), '-',
+//     tokens['response-time'](req, res), 'ms'
+//   ].join(' '),
+//   res.json()
+// })
+morgan.token('body', function (req) { return JSON.stringify(req.body)})
+app.use(morgan(':method :url :status :response-time :body'))
 // phonebook
 let phonebook = [
   {
