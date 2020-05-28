6fb96bed340378a5abb53aa7226176132c896698
Blakells <blake.bailey09@gmail.com>
2020-05-28 06:53:36 -0400

    7a

--- Part-7/counter/src/App.js
+++ Part-7/counter/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Route, Switch, Link} from 'react-router-dom'
+import {Route, Switch, Link, useRouteMatch, useHistory, Redirect} from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -9,21 +9,69 @@ const Home = () => {
   )
 }
 
-const Note = ({ notes }) => {
+const Note = ({note})=> {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : '' }</strong></div>
+    </div>
+  )
+}
+
+const Notes = ({ notes }) => {
   return (
     <div>
       <h2>Notes</h2>
       <ul>
         {notes.map(note =>
           <li key={note.id}>
-            <Link to={}>{note.content}</Link>
+            <Link to={`/notes/${note.id}`}>{note.content}</Link>
           </li>)}
       </ul>
     </div>
   )
 }
 
+const Users = () => {
+  return (
+    <div>
+      <h2>Users</h2>
+      <p>User 1</p>
+      <p>User 2</p>
+    </div>
+  )
+}
+
+const Login = (props) => {
+  const history = useHistory()
+  let e = ''
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin(e)
+    history.push('/')
+  }
+const handleChange = (event) => {
+  e = event.target.value
+}
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          username : <input id='user' onChange={handleChange}/>
+        </div>
+        <div>
+          password : <input type='password'/>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
+
   const [notes, setNotes] = useState([
     {
       id: 1,
@@ -45,6 +93,14 @@ const App = () => {
     }
   ])
 
+  let match = useRouteMatch('/notes/:id')
+  const note = match ? notes.find(note => note.id === Number(match.params.id)) : null
+
+  const [user,setUser] = useState(null)
+
+  const login = (user) => {
+    setUser(user)
+  }
 
   const padding = {
     padding: 5
@@ -56,13 +112,20 @@ const App = () => {
       <Link style={padding} to='/'>home</Link>
       <Link style={padding} to='/notes'>notes</Link>
       <Link style={padding} to='/users'>users</Link>
+      {user ? <h4>{user} logged in</h4> : <Link to='/login'>login</Link>}
     </div>
     <Switch>
+      <Route path='/notes/:id'>
+        <Note note={note}/>
+      </Route>
       <Route path='/notes'>
-        <Notes />
+        <Notes notes={notes}/>
       </Route>
       <Route path='/users'>
-        <Users/>
+        {user ? <Users /> : <Redirect to='/login'/>}
+      </Route>
+      <Route path='/login'>
+        <Login onLogin={login}/>
       </Route>
       <Route path='/'>
         <Home />
